d6ecb3ed88f9e5ebeadd3ca135fd32ef66c4f622
filipe custodio <filipe.l.custodio@gmail.com>
2018-11-28 19:36:04 +0000

    restaurant and review done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '914945314',
+    category: 'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '914858491',
+    category: 'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+
+puts 'Creating reviews'
+reviews_attributes = [
+  {
+    restaurant_id: 1,
+    content: "Blablablablablabla",
+    rating: 5
+  },
+  {
+    restaurant_id: 1,
+    content: "Blablablablablabla",
+    rating: 3
+  }
+]
+Review.create!(reviews_attributes)
+puts 'Finished!'
+
